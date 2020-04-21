ac984ae91542df6677b018518bfffc251849dccc
jakubsaleniuk <jakub.saleniuk@gmail.com>
2020-04-21 14:07:00 +0200

    Added README, missing dependencies, .env.example and eslint. Fixed errors in functionalities.

---
+++ README.md
@@ -0,0 +1,33 @@
+## Available Scripts
+
+Before starting the project: copy `.env.example` to `.env` and set API_URL
+
+In the project directory, you can run:
+
+### `expo start`
+
+Runs the app in the development mode.<br />
+
+The page will reload if you make edits.<br />
+You will also see any errors in the console.
+
+### `expo ios`
+
+Open iOS simulator and runs application (can be used when server is started (`expo start`))
+
+### `expo android`
+
+Open Android simulator and runs application (can be used when server is started (`expo start`))
+
+### `yarn test`
+
+Launches the test runner in the interactive watch mode.<br />
+
+## Building standalone apps
+
+https://docs.expo.io/versions/latest/distribution/building-standalone-apps/
+
+## Tested on:
+
+- iPhone 8 and iPhone 11
+- Android R (Pixel 2)
--- src/videos/screens/Video.js
+++ src/videos/screens/Video.js
@@ -1,43 +1,51 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Query } from 'react-apollo';
 import { Video as ExpoVideo } from 'expo-av';
 import { useRoute } from '@react-navigation/native';
-import { useQuery } from "@apollo/react-hooks";
 import * as ScreenOrientation from 'expo-screen-orientation';
 import { GET_VIDEO } from '../api/queries';
 
 export default function Video() {
-
   const route = useRoute();
-  const { data, loading, error } = useQuery(GET_VIDEO, { variables: { videoId: route.params.videoId } });
-
   React.useEffect(() => {
     ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
-
-    return () => {
-      ScreenOrientation.unlockAsync();
-    }
+    return () => ScreenOrientation.unlockAsync();
   }, []);
 
-  if (loading) return <Text>Loading...</Text>;
-  if (error) return <Text>Error</Text>;
-  if (!data) return <Text>Not found</Text>;
-
   return (
-    <View style={styles.container}>
-      <ExpoVideo
-        source={{ uri: data.video.url }}
-        rate={1.0}
-        volume={1.0}
-        isMuted={false}
-        resizeMode="cover"
-        shouldPlay
-        useNativeControls={true}
-        style={styles.video}
-      />
-      <Text style={styles.title}>{data.video.title}</Text>
-    </View>
-  )
+    <Query query={GET_VIDEO} variables={{ videoId: route.params.videoId }}>
+      {({ loading, error, data }) => {
+        if (loading) return <Text testID="loading">Loading...</Text>;
+        if (error) {
+          return (
+            <Text>
+              `Error! $
+              {error.message}
+              `
+            </Text>
+          );
+        }
+
+        return (
+          <View style={styles.container}>
+            <ExpoVideo
+              source={{ uri: data.video.url }}
+              rate={1.0}
+              volume={1.0}
+              isMuted={false}
+              resizeMode="cover"
+              shouldPlay
+              useNativeControls
+              style={styles.video}
+            />
+            <Text style={styles.title}>{data.video.title}</Text>
+          </View>
+        );
+      }}
+    </Query>
+
+  );
 }
 
 const styles = StyleSheet.create({
